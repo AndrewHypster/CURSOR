b8e96f94d5ae4adfba476a07dd5aadcbff7a8f6b
Андрій Гречух <119750751+AndrewHypster@users.noreply.github.com>
2023-03-02 11:27:25 +0200

    Update script.js

--- HW01/script.js
+++ HW01/script.js
@@ -1,82 +1,46 @@
-const div = document.querySelector('.text');
-let inner ="";
-
-function consoleLog (text) {
-    inner += text;
-    div.innerHTML = inner;
-}
-
 // Товари
-// let egg = 15.678,
-//     book = 123.965,
-//     butter = 90.2345;
+let bun = 15.678,
+    book = 123.965,
+    butter = 90.2345;
 
-let goods = ['булочка', 'книжка', 'масло'],     // товари
-    price = [15.678, 123.965, 90.2345];   // ціна
-
-consoleLog(`<b>Товари</b> <br/> ${goods[0]} = ${price[0]}; ${goods[1]} = ${price[1]}; ${goods[2]} = ${price[2]}. <br/>`);
+console.log(`* Товари *\n\nБулка = ${bun}\nКнижка = ${book}\nМасло = ${butter}`);
 
 // Max
-const max = Math.max(price[0], price[1], price[2]);
-
-if (max == price[0]) 
-    consoleLog(`<br/> Max: ${goods[0]} = ${price[0]}`);
-else if (max == price[1]) 
-    consoleLog(`<br/> Max: ${goods[1]} = ${price[1]}`);
-else if (max == price[2]) 
-    consoleLog(`<br/> Max: ${goods[2]} = ${price[2]}`);
-else
-    consoleLog(`<br/> Max: ERROR`);
+console.log('Найдорожче: ' + Math.max(bun, book, butter));
 
 // Min
-const min = Math.min(price[0], price[1], price[2]);
-
-if (min == price[0]) 
-consoleLog(`<br/> Min: ${goods[0]} = ${price[0]}`);
-else if (min == price[1]) 
-    consoleLog(`<br/> Min: ${goods[1]} = ${price[1]}`);
-else if (min == price[2]) 
-    consoleLog(`<br/> Min: ${goods[2]} = ${price[2]}`);
-else
-    consoleLog(`<br/> Min: ERROR`);
+console.log('Найдешевше: ' + Math.min(bun, book, butter));
 
 // Sum
-consoleLog('<br/> Сума: ' + (price[0] + price[1] + price[2]));
+const sum = bun + book + butter;
+console.log('Сума: ' + (sum));
 
-// Floor and sum
-let floor = [Math.floor(price[0]), Math.floor(price[1]), Math.floor(price[2])]  // масив з заокругленою ціною
-consoleLog('<br/> Floor та сума: ' + (floor[0] + floor[1] + floor[2])); // додаємо та виводимо
+// Floor та сума
+const floorSum = (Math.floor(bun) + Math.floor(book) + Math.floor(butter));
+console.log('Floor та сума: ' + floorSum);
 
-// Округляння до сотень та сума
-let hundreds = [];    // округлені ціни то сотень
-for (let i = 0; i <= floor.length; i++) {
-    hundreds[i] =  floor[i] / 100;
-    hundreds[i] = Math.ceil(hundreds[i]);   // округлення до більшого
-    hundreds[i] = hundreds[i] * 100;
-}
-consoleLog(`<br/> Округляння до сотень та сума: ${hundreds[0] + hundreds[1] + hundreds[2]}`);
+// Округлення до сотень
+console.log('Округлення до сотень: ' + (Math.round(((Math.round(bun) + Math.round(book) + Math.round(butter)))/100))*100);
 
 // Буля
-const isEven = ((floor[0] + floor[1] + floor[2]) % 2) == 0 ? 'число парне':'число не парне';
-consoleLog(`<br/> ${isEven}`);
+const isEven = ((floorSum) % 2) == 0 ? 'число парне':'число не парне';
+console.log(isEven);
 
 // Решта
-if ((price[0] + price[1] + price[2]) <= 500)
-    consoleLog(`<br/> Решта: ${500 - (price[0] + price[1] + price[2])}`); // проста сума - floor сума
+console.log(`Решта з 500: ${500 - (sum)}`);
 
 // Середнє
-consoleLog('<br/> Середнє значення: '+((price[0] + price[1] + price[2])/3).toFixed(2));
+console.log('Середнє значення: '+((sum)/3).toFixed(2));
 
-consoleLog('<br/><br/><b>Завдання з зірочкою</b><br/><br/>');
+console.log('\n* Завдання з зірочкою *');
 
 // Знижка
 const discount = Math.round(Math.random()*50); // знижка від 0 до 50
-consoleLog(`Знижка: ${discount}% <br/>`);
-const discountedPrice = ((price[0] + price[1] + price[2])*(discount/100)).toFixed(2);
-consoleLog('Ціна зі знижкою: '+ discountedPrice +'<br/>');
-consoleLog(`Прибуток: ${(((price[0] + price[1] + price[2])/2)-(discountedPrice)).toFixed(2)}`);
+console.log(`\nЗнижка: ${discount}%`);
+const discountedPrice = ((sum)*(discount/100)).toFixed(2);
+console.log('Ціна зі знижкою: ' + discountedPrice);
+console.log(`Прибуток: ${(((sum)/2)-(discountedPrice)).toFixed(2)}`);
 
 // Шаблонний рядок
-consoleLog('<br/><br/><b>Шаблонний рядок</b><br/><br/>');
-
-consoleLog(`Максимальна ціна: ${max} Мінімальна ціна: ${min} Вартість всіх товарів: ${(price[0] + price[1] + price[2])} Вартість з округленням: ${(floor[0] + floor[1] + floor[2])} Округлення до сотень: ${hundreds[0] + hundreds[1] + hundreds[2]} Чи вартість товарів є парним числом: ${isEven} Здача з 500: ${500 - (price[0] + price[1] + price[2])} Середнє значення: ${(price[0] + price[1] + price[2]).toFixed(2)} Знижка: ${discount}% Ціна зі знижкою: ${discountedPrice} Прибуток: ${(((price[0] + price[1] + price[2])/2)-(discountedPrice)).toFixed(2)}`);
+console.log('\n* Шаблонний рядок *');
+console.log(`Максимальна ціна: ${Math.max(bun, book, butter)}\nМінімальна ціна: ${Math.min(bun, book, butter)}\nВартість всіх товарів: ${sum}\nВартість з округленням: ${(Math.floor(bun) + Math.floor(book) + Math.floor(butter))}\nОкруглення до сотень: ${(Math.round(((Math.round(bun) + Math.round(book) + Math.round(butter)))/100))*100}\nЧи вартість товарів є парним числом: ${isEven}\nЗдача з 500: ${500 - (sum)}\nСереднє значення: ${(sum).toFixed(2)}\nЗнижка: ${discount}%\nЦіна зі знижкою: ${discountedPrice}\nПрибуток: ${(((sum)/2)-(discountedPrice)).toFixed(2)}`);
