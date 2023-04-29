eff78f4b6a5dbed9d2181d95b818856068a5583f
Андрій Гречух <119750751+AndrewHypster@users.noreply.github.com>
2023-04-29 12:08:40 +0300

    Delete HW15/mvs directory

--- HW15/mvs/html.js
+++
@@ -1,6 +0,0 @@
-/** С Т В О Р Ю Є М О   К О Ж Н І Й   Р О Б О Т І   С В І Й   Д І В **/
-for (let i=1; i<=13; i++) document.body.innerHTML += `<div class="${i}" id="HW"><h3>HW ${i}</h3></div>`;
-const HWs = [null];
-document.querySelectorAll('#HW').forEach((div) => HWs.push(div));
-
-export default HWs;
