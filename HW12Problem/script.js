957de59c0021c3e7429133e04169951e6dd4ee6e
Андрій Гречух <119750751+AndrewHypster@users.noreply.github.com>
2023-05-01 12:38:52 +0300

    Delete HW12Problem directory

--- HW12Problem/script.js
+++
@@ -1,9 +0,0 @@
-document.body.innerHTML += '<h1 class="first">first</h1><h1 class="second">second</h1>';
-const first = document.querySelector('.first');
-const second = document.querySelector('.second');
-
-first.addEventListener('click', () => console.log('first'));
-second.addEventListener('click', () => {
-    console.log('add text');
-    document.body.innerHTML += '<p>текст</p>'
-});
