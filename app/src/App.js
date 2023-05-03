ea9a00174f491cb77898e27ba539c76c34b11572
Андрій Гречух <119750751+AndrewHypster@users.noreply.github.com>
2023-05-03 12:00:38 +0300

    Add files via upload

---
+++ app/src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Hello
+        </p>
+        <a>
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
